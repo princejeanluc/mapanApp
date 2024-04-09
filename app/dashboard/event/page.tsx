410163289e86765d8a34ec0a8bcb5a3490715c69
princejeanluc <princejeanluc.denteppe@gmail.com>
2024-04-09 11:52:58 +0100

    fixing bugs for linsting events

--- app/dashboard/event/page.tsx
+++ app/dashboard/event/page.tsx
@@ -1,16 +1,14 @@
 'use client'
 import SearchComponent from "@/app/ui/dashboard/event/SearchComponent";
-import JumbotronImage from "@/app/ui/dashboard/event/JumbotronImage";
-import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import {useContext} from "react";
 import {EventsFromOwnerContext} from "@/app/dashboard/layout";
+import EventList from "@/app/ui/dashboard/event/EventList";
 
 
 export default function Page(){
 
     const events:any = useContext(EventsFromOwnerContext)
-    console.log(events)
-    // @ts-ignore
+
     return <>
         <div >
             <div className={`h-20 p-5 `}>
@@ -21,21 +19,7 @@ export default function Page(){
 
             <div className={`p-5 md:p-10 h-full`}>
 
-                <ResponsiveMasonry columnsCountBreakPoints={{350: 2, 750: 2, 900: 3}} >
-                    <Masonry gutter="10px">
-                        {events ? events.map((event:any, i:number) => (
-                            <div className={``} key={event.pk}>
-                                <img className={`rounded `}
-                                    key={i}
-                                    src={"http://127.0.0.1:8000/" + event.image}
-                                    style={{width: "100%", display: "block"}}
-                                />
-                            <div className={`relative block top-0 left-0 w-4/5 bg-amber-300`}></div>
-                            </div>
-
-                        )) : <div></div>}
-                    </Masonry>
-                </ResponsiveMasonry>
+                <EventList evs={events}></EventList>
             </div>
         </div>
     </>
--- app/ui/dashboard/event/EventList.tsx
+++ app/ui/dashboard/event/EventList.tsx
@@ -1,72 +1,23 @@
-export default function EventList(){
+'use client'
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 
-    const posts = [
-        {
-            title: "What is SaaS? Software as a Service Explained",
-            desc: "Going into this journey, I had a standard therapy regimen, based on looking at the research literature. After I saw the movie, I started to ask other people what they did for their anxiety, and some",
-            img: "https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            authorLogo: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
-            authorName: "Sidi dev",
-            date: "Jan 4 2022",
-            href: "javascript:void(0)"
-        },
-        {
-            title: "A Quick Guide to WordPress Hosting",
-            desc: "According to him, â€œI'm still surprised that this has happened. But we are surprised because we are so surprised.â€More revelations about Whittington will be featured in the film",
-            img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            authorLogo: "https://api.uifaces.co/our-content/donated/FJkauyEa.jpg",
-            authorName: "Micheal",
-            date: "Jan 4 2022",
-            href: "javascript:void(0)"
-        },
-        {
-            title: "7 Promising VS Code Extensions Introduced in 2022",
-            desc: "I hope I remembered all the stuff that they needed to know. They're like, 'okay,' and write it in their little reading notebooks. I realized today that I have all this stuff that",
-            img: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            authorLogo: "https://randomuser.me/api/portraits/men/46.jpg",
-            authorName: "Luis",
-            date: "Jan 4 2022",
-            href: "javascript:void(0)"
-        },
-        {
-            title: "How to Use Root C++ Interpreter Shell to Write C++ Programs",
-            desc: "The powerful gravity waves resulting from the impact of the planets' moons â€” four in total â€” were finally resolved in 2015 when gravitational microlensing was used to observe the",
-            img: "https://images.unsplash.com/photo-1617529497471-9218633199c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            authorLogo: "https://api.uifaces.co/our-content/donated/KtCFjlD4.jpg",
-            authorName: "Lourin",
-            date: "Jan 4 2022",
-            href: "javascript:void(0)"
-        }
-    ]
+// @ts-ignore
+export default function EventList({evs}){
 
-    return (
-        <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8">
-            <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
-                {
-                    posts.map((items, key) => (
-                        <article className="bg-white max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
-                            <a href={items.href}>
-                                <img src={items.img} loading="lazy" alt={items.title}  className="w-full h-48 rounded-t-md" />
-                                <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
-                                    <div className="flex-none w-10 h-10 rounded-full">
-                                        <img src={items.authorLogo} className="w-full h-full rounded-full" alt={items.authorName} />
-                                    </div>
-                                    <div className="ml-3">
-                                        <span className="block text-gray-900">{items.authorName}</span>
-                                        <span className="block text-gray-400 text-sm">{items.date}</span>
-                                    </div>
-                                </div>
-                                <div className="pt-3 ml-4 mr-2 mb-3">
-                                    <h3 className="text-xl text-gray-900">
-                                        {items.title}
-                                    </h3>
-                                    <p className="text-gray-400 text-sm mt-1">{items.desc}</p>
-                                </div>
-                            </a>
-                        </article>
-                    ))
-                }
-            </div>
-        </section>
-    )
+    return <>
+        <ResponsiveMasonry columnsCountBreakPoints={{350: 2, 750: 2, 900: 3}} >
+            <Masonry gutter="10px">
+                {evs ? evs.map((event:any, i:number) => (
+                    <div className={``} key={event.pk}>
+                        <img className={`rounded `}
+                             key={i}
+                             src={"http://127.0.0.1:8000/" + event.image}
+                             style={{width: "100%", display: "block"}}
+                        />
+                        <div className={`relative block top-0 left-0 w-4/5 bg-amber-300`}></div>
+                    </div>
+
+                )) : <div></div>}
+            </Masonry>
+        </ResponsiveMasonry></>
 }
